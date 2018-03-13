76f623360895e10a1b7b686a7af29a7d6f053926
DaVuD321 <davudhrab@gmail.com>
2018-03-13 12:04:59 +0100

    good job

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -5,8 +5,14 @@
 #include <serial_port.h>
 #include <numbers_parser.h>
 
+//EventMeassurment whole_lap(1, 2), top_speed(3, 4);
+
 void process_result(std::vector<float> &result)
 {
+  /*
+  whole_lap.process(result);
+  top_speed.process(result);
+  */
   printf("result : ");
   for (unsigned int i = 0; i < result.size(); i++)
     printf("%6.3f ", result[i]);
@@ -29,6 +35,13 @@ int main()
 
   while (1)
   {
+    /*
+    if (get_char() == ' ')
+    {
+      whole_lap.reset();
+      top_speed.reset();
+    }
+    */
     if (serial_port.is_char())
     {
       parser.add(serial_port.get_char());
@@ -42,7 +55,20 @@ int main()
 
     }
     else
+    {
+      /*
+      if (whole_lap.is_done())
+      {
+        printf("lap time %f[s]\n", whole_lap.get());
+      }
+
+      if (top_speed.is_done())
+      {
+        printf("top speed  %f[s]\n", top_speed.get());
+      }
+      */
       usleep(100*1000);
+    }
   }
 
   printf("program done\n");
